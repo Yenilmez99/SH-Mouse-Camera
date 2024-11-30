a98cf4d32b8d85088aeb58747f27765f4ccf8d0c
Yenilmez99 <178713721+Yenilmez99@users.noreply.github.com>
2024-11-30 17:10:50 +0300

    v1.2.0

--- Sonic Heroes Mouse Camera/Sonic Heroes Mouse Camera/dllmain.cpp
+++ Sonic Heroes Mouse Camera/Sonic Heroes Mouse Camera/dllmain.cpp
@@ -1,12 +1,14 @@
-﻿#include <windows.h>
-#include <tlhelp32.h>
-#include <tchar.h>
-#include <vector>
-#include <stdlib.h>
-#include <math.h>
-#include <string>
+﻿#include "pch.h"
 
-#include "framework.h"
+#define PiNumber 3.1415
+#define OnOffKey 0x4E // N key
+#define ForceCloseKey 0x55 // U key
+#define SensUpKey 0x72 // F3 Key
+#define SensDownKey 0x73 // F4 Key
+#define SensResetKey 0x74 // F5 Key
+#define RadiusUpKey 0x75 // F6 Key
+#define RadiusDownKey 0x76 // F7 Key
+#define RadiusResetKey 0x77 // F8 Key
 
 DWORD GetModuleBaseAddress(TCHAR* lpszModuleName, DWORD pID) {
     DWORD dwModuleBaseAddress = 0;
@@ -35,7 +37,6 @@ DWORD GetPointerAddress(HWND hwnd, DWORD gameBaseAddr, DWORD address, std::vecto
     HANDLE phandle = NULL;
     phandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
     if (phandle == INVALID_HANDLE_VALUE || phandle == NULL);
-
     DWORD offset_null = NULL;
     ReadProcessMemory(phandle, (LPVOID*)(gameBaseAddr + address), &offset_null, sizeof(offset_null), 0);
     DWORD pointeraddress = offset_null;
@@ -47,21 +48,22 @@ DWORD GetPointerAddress(HWND hwnd, DWORD gameBaseAddr, DWORD address, std::vecto
 }
 
 DWORD WINAPI MainCore(HMODULE hModule) {
-    bool RunStage = 0, GamePause = 0, CameraFreeze = 0, CameraUnFreeze = 1;
-    short int CameraRotationV = 0, CameraRotationH = 0;
-    int MousePosXDelta = 0, MousePosYDelta = 0;
-    const float SensivityV = 0.2f, SensivityH = 0.3f;
-    const float Radius = 140.0f;
-    float CharacterXPos = 0.0f, CharacterYPos = 0.0f, CharacterZPos = 0.0f;
-    float CameraXPos = 0.0f, CameraYPos = 0.0f, CameraZPos = 0.0f;
-    float DegreeAlpha = 0.0f, DegreeBeta = 0.0f;
 
-    POINT MousePos;
+    bool StageOpen = 1, OnOff = 1, CameraFreeze = 0, GamePause = 0;
+
+    short int CameraRotation[2] = { 0,0 };
+    int MousePosDifference[2] = { 0,0 };
+
+    float CharacterPos[3] = { 0.0f,0.0f,0.0f }, CameraPosition[3] = { 0.0f,0.0f,0.0f };
+    float Sensivity = 10.0f, Radius = 90.0f;
+
+    POINT MousePosition;
 
     HWND hwnd_SonicHeroesTM = FindWindowA(NULL, "SONIC HEROES(TM)");
 
     while (hwnd_SonicHeroesTM == NULL) {
         hwnd_SonicHeroesTM = FindWindowA(NULL, "SONIC HEROES(TM)");
+        Sleep(100);
     }
 
     DWORD ProcessIDSonicHeroes = NULL;
@@ -70,66 +72,90 @@ DWORD WINAPI MainCore(HMODULE hModule) {
     char SonicHeroesGameModule[] = "Tsonic_win.exe";
     DWORD SonicHeroesMainAdress = GetModuleBaseAddress(_T(SonicHeroesGameModule), ProcessIDSonicHeroes);
 
-    DWORD CharacterXPosMainAdres = 0x005CE820;
+    DWORD CharacterPosMainAdres = 0x005CE820;
 
-    std::vector<DWORD> CharacterXPosOffset{ 0xE8 };
+    std::vector<DWORD> CharacterPosOffset{ 0xE8 };
 
-    DWORD UseCharacterXPosAdres;
-    DWORD UseCameraFreezeAdres;
+    DWORD CharacterPosAdres = GetPointerAddress(hwnd_SonicHeroesTM, SonicHeroesMainAdress, CharacterPosMainAdres, CharacterPosOffset);
 
-    while (true) {
-        ReadProcessMemory(HandleSonicHeroes, (PBYTE*)0x007C6BD4, &RunStage, sizeof(bool), 0);
-        ReadProcessMemory(HandleSonicHeroes, (PBYTE*)0x008D6708, &GamePause, sizeof(bool), 0);
+    ReadProcessMemory(HandleSonicHeroes, (PBYTE*)0x007C6BD4, &StageOpen, sizeof(bool), 0);
+    ReadProcessMemory(HandleSonicHeroes, (PBYTE*)0x008D6708, &GamePause, sizeof(bool), 0);
 
-        if (RunStage == 0 && GamePause == 0 && GetAsyncKeyState(0x4E) == 0){
-            UseCharacterXPosAdres = GetPointerAddress(hwnd_SonicHeroesTM, SonicHeroesMainAdress, CharacterXPosMainAdres, CharacterXPosOffset);
+    SetCursorPos(800, 600);
 
-            ReadProcessMemory(HandleSonicHeroes, (PBYTE*)UseCharacterXPosAdres, &CharacterXPos, sizeof(float), 0);
-            ReadProcessMemory(HandleSonicHeroes, (PBYTE*)(UseCharacterXPosAdres + 0x4), &CharacterYPos, sizeof(float), 0);
-            ReadProcessMemory(HandleSonicHeroes, (PBYTE*)(UseCharacterXPosAdres + 0x8), &CharacterZPos, sizeof(float), 0);
+    while (hwnd_SonicHeroesTM != 0) {
+        // Camera Freeze
+        WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A69880, &CameraFreeze, sizeof(bool), 0);
 
-            GetCursorPos(&MousePos);
+        // Stage and Window ON
+        if (StageOpen == 0 && GamePause == 0 && OnOff == 1 && hwnd_SonicHeroesTM == GetForegroundWindow()) {
+            GetCursorPos(&MousePosition);
+            MousePosDifference[0] += MousePosition.x - 800;
+            MousePosDifference[1] += MousePosition.y - 600;
             SetCursorPos(800, 600);
 
-            MousePosXDelta += 800 - MousePos.x;
-            MousePosYDelta += MousePos.y - 600;
+            if (MousePosDifference[0] * Sensivity > 65536)
+                MousePosDifference[0] = (MousePosDifference[0] % 65536) / Sensivity;
 
-            if (MousePosYDelta * SensivityV > 89) {
-                MousePosYDelta = 89 / SensivityV;
-            }
-            else if (MousePosYDelta * SensivityV < -89) {
-                MousePosYDelta = -89 / SensivityV;
-            }
-            else{
-            }
+            if (MousePosDifference[1] * Sensivity > 16384)
+                MousePosDifference[1] = 16384 / Sensivity;
 
-            CameraXPos = CharacterXPos + Radius * cos(SensivityV * MousePosYDelta * 3.14f / 180) * sin(SensivityH * MousePosXDelta * 3.14f / 180);
-            CameraYPos = CharacterYPos + Radius * sin(SensivityV * MousePosYDelta * 3.14f / 180);
-            CameraZPos = CharacterZPos + Radius * cos(SensivityV * MousePosYDelta * 3.14f / 180) * cos(SensivityH * MousePosXDelta * 3.14f / 180);
-            CameraRotationH = SensivityH * MousePosXDelta * 182;
-            CameraRotationV = - SensivityV * MousePosYDelta * 182;
+            if (MousePosDifference[1] * Sensivity < -16384)
+                MousePosDifference[1] = -16384 / Sensivity;
 
-            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A69880, &CameraFreeze, sizeof(bool), 0); // Freeze Camera
-            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C30, &CameraXPos, sizeof(float), 0);
-            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C34, &CameraYPos, sizeof(float), 0);
-            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C38, &CameraZPos, sizeof(float), 0);
-            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C40, &CameraRotationH, sizeof(short int), 0);
-            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C3C, &CameraRotationV, sizeof(short int), 0);
-        }
-        else{
+            CharacterPosAdres = GetPointerAddress(hwnd_SonicHeroesTM, SonicHeroesMainAdress, CharacterPosMainAdres, CharacterPosOffset);
+            ReadProcessMemory(HandleSonicHeroes, (PBYTE*)CharacterPosAdres, &CharacterPos[0], sizeof(float), 0);
+            ReadProcessMemory(HandleSonicHeroes, (PBYTE*)(CharacterPosAdres + 0x4), &CharacterPos[1], sizeof(float), 0);
+            ReadProcessMemory(HandleSonicHeroes, (PBYTE*)(CharacterPosAdres + 0x8), &CharacterPos[2], sizeof(float), 0);
+
+            CameraPosition[0] = CharacterPos[0] + Radius * cos(MousePosDifference[1] * Sensivity * PiNumber / 32768) * cos(MousePosDifference[0] * Sensivity * PiNumber / 32768);
+            CameraPosition[1] = 8.5f + CharacterPos[1] + Radius * sin(MousePosDifference[1] * Sensivity * PiNumber / 32768);
+            CameraPosition[2] = CharacterPos[2] + Radius * cos(MousePosDifference[1] * Sensivity * PiNumber / 32768) * sin(MousePosDifference[0] * Sensivity * PiNumber / 32768);
+            
+            CameraRotation[0] = -Sensivity * MousePosDifference[0] + 16384;
+            CameraRotation[1] = -Sensivity * MousePosDifference[1];
+
+            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C30, &CameraPosition[0], sizeof(float), 0);
+            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C34, &CameraPosition[1], sizeof(float), 0);
+            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C38, &CameraPosition[2], sizeof(float), 0);
+
+            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C40, &CameraRotation[0], sizeof(short int), 0);
+            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A60C3C, &CameraRotation[1], sizeof(short int), 0);
+
+            CameraFreeze = 0;
         }
 
-        if (GetAsyncKeyState(0x55)) {
-            break;
-        }
-        else if (GetAsyncKeyState(0x4E) != 0) {
-            WriteProcessMemory(HandleSonicHeroes, (PBYTE*)0x00A69880, &CameraUnFreeze, sizeof(bool), 0);
-        }
+        // Stage and Window OFF
         else {
+            CameraFreeze = 1;
         }
 
-        Sleep(10);
+        if (GetAsyncKeyState(OnOffKey) & 1)
+            OnOff = !OnOff;
 
+        if (GetAsyncKeyState(ForceCloseKey) & 1)
+            break;
+
+        if (GetAsyncKeyState(SensUpKey) & 1)
+            Sensivity += 1;
+        if (GetAsyncKeyState(SensDownKey) & 1)
+            if (Sensivity > 0)
+                Sensivity -= 1;
+        if (GetAsyncKeyState(SensResetKey) & 1)
+            Sensivity = 10.0f;
+        if (GetAsyncKeyState(RadiusUpKey) & 1)
+            Radius += 5;
+        if (GetAsyncKeyState(RadiusDownKey) & 1)
+            if (Radius > 0)
+                Radius -= 5;
+        if (GetAsyncKeyState(RadiusResetKey) & 1)
+            Radius = 90.0f;
+
+        // Controls
+        Sleep(10);
+        ReadProcessMemory(HandleSonicHeroes, (PBYTE*)0x007C6BD4, &StageOpen, sizeof(bool), 0);
+        ReadProcessMemory(HandleSonicHeroes, (PBYTE*)0x008D6708, &GamePause, sizeof(bool), 0);
+        hwnd_SonicHeroesTM = FindWindowA(NULL, "SONIC HEROES(TM)");
     }
 
     return 0;
