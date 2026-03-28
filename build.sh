7bcf5e4cfa15b9a83841a4313b5a330d3e2701d1
Yenilmez99 <178713721+Yenilmez99@users.noreply.github.com>
2026-03-28 21:22:19 +0300

    Update build.sh
    build.sh was edited for Release build

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "== C++ DLL is being compiled =="
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=mingw32-toolchain.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+BUILD_TYPE=${1:-Release}
+
+echo "== C++ DLL is being compiled ($BUILD_TYPE) =="
+cmake -B build -DCMAKE_TOOLCHAIN_FILE=mingw32-toolchain.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 cmake --build build
 echo "== C++ DLL was compiled =="
 
-echo "== C# Config DLL is being compiled =="
+echo "== C# Config DLL is being compiled ($BUILD_TYPE) =="
 dotnet build SH-Mouse-Camera.csproj -c Release
 echo "== C# DLL was compiled =="
 echo "== All Project were compiled successfully=="
